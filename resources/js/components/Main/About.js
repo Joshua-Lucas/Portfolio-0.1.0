38b46de95a0930472b2203e84bb39d30edc5ddc7
Joshua Lucas <joshuamlucas91@gmail.com>
2020-04-24 19:38:12 -0400

    add main and about components and imported them to app.js

---
+++ resources/js/components/Main/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const About = () => 
+{
+    return (
+        <div className=" lg:-mt-20 bg-accent h-full">
+           <p className="pt-20">Hi my name is Joshua</p>  
+        </div>
+
+    );
+
+
+
+}
+
+export default About;
---
+++ resources/js/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import About from './About';
+
+
+const Main = () => 
+{
+    return (
+        <React.Fragment>
+            <About />
+        </React.Fragment>
+    );
+
+}
+
+export default Main;
