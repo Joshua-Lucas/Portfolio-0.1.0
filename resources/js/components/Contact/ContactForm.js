f2371fad0b0be56b33aa362b2ff0908b0773030c
Joshua Lucas <joshuamlucas91@gmail.com>
2020-05-12 17:57:50 -0400

    Work on error with json.stringify()

--- resources/js/components/Contact/ContactForm.js
+++ resources/js/components/Contact/ContactForm.js
@@ -4,10 +4,10 @@ import React, { useReducer } from "react";
 const ContactForm = () =>
 {
     const initialState = {
-            name: '', 
-            email: '',
-            reason:'', 
-            message: ''
+            name: "", 
+            email: "",
+            reason:"", 
+            message: ""
     };
 
    const  reducer = (state, {field, value}) =>  {
@@ -29,9 +29,14 @@ const ContactForm = () =>
     const handleOnSubmit = (e) =>
     {
         e.preventDefault();
-        console.log(state);
+        console.log(JSON.stringify(state));
+        fetch('api/contact' , {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify(state)
+        })
+        .then(resp => resp.json())
         
-
     }
 
     return (
@@ -39,10 +44,10 @@ const ContactForm = () =>
             <div>
                 <h1>Test</h1>
                 <form className="flex flex-col space-y-5" onSubmit={handleOnSubmit}>
-                    <input type='text' name='name' value={name} onChange={handleChange} placeholder='Enter Name'/>
-                    <input type='text' name='email'  value={email} onChange={handleChange} placeholder='Enter Email' />
-                    <input type='text' name='reason' value={reason} onChange={handleChange} placeholder='Select Reason for Contacting'/>
-                    <textarea type="text" name='message' value={message} onChange={handleChange} placeholder='Enter Message'/>
+                    <input type='text' name="name" value={name} onChange={handleChange} placeholder='Enter Name'/>
+                    <input type='email' name="email"  value={email} onChange={handleChange} placeholder='Enter Email' />
+                    <input type='text' name="reason" value={reason} onChange={handleChange} placeholder='Select Reason for Contacting'/>
+                    <textarea type="text" name="message" value={message} onChange={handleChange} placeholder='Enter Message'/>
 
                     <button type="submit">Send</button>
                 </form>
