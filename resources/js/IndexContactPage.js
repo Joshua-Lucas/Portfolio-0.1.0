bcedf443d44a30625824d49db1cb846fb556c232
Joshua Lucas <joshuamlucas91@gmail.com>
2020-05-14 16:57:20 -0400

    Renamed and moved contactPage to aviod confusion.

--- resources/js/IndexContactPage.js
+++ resources/js/IndexContactPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ContactPage from './Contact/ContactPage';
+import ContactPage from './components/Contact/ContactPage';
 
 if (document.getElementById('contactPage')) {
     ReactDOM.render(<ContactPage />, document.getElementById('contactPage'));
