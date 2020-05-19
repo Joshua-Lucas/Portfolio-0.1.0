7a463ced601611ef5fe3769a08fd212167ae8915
Joshua Lucas <joshuamlucas91@gmail.com>
2020-05-19 15:29:56 -0400

    Removed  files no longer used

--- resources/js/IndexContactPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ContactPage from './components/Contact/ContactPage';
-
-if (document.getElementById('contactPage')) {
-    ReactDOM.render(<ContactPage />, document.getElementById('contactPage'));
-}
